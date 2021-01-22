2ad673b5f6cca1c85c375ad57edc74661e93eb61
niilo23 <56909449+niilo23@users.noreply.github.com>
2021-01-22 12:42:28 +0200

    albumi tehtävä tehty

---
+++ Harkat/Harjoitus 4 (KT)/AlbumiHarjoitus/Albumi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlbumiHarjoitus
+{
+    class Albumi
+    {
+        public string artisti { get; set; }
+        public string musiikinNimi { get; set; }
+        public string genre { get; set; }
+        public int hinta { get; set; }
+
+        public List<string> Kappale = new List<string>(10);
+        
+        public Albumi (string _artisti, string _musiikki, string _genre, int _hinta)
+        {
+            artisti = _artisti;
+            musiikinNimi = _musiikki;
+            genre = _genre;
+            hinta = _hinta;
+        }
+
+        public void TulostaAlbumiData()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Artisti: " + artisti);
+            Console.WriteLine("Nimi: " + musiikinNimi);
+            Console.WriteLine("Genre: " + genre);
+            Console.WriteLine("Hinta: " + hinta + "e");
+            Console.WriteLine("\n" + "Songs");
+        }
+
+        public void lisääKappale(string Nimi)
+        {
+            Kappale.Add(Nimi);
+        }
+
+        public void TulostaKappaleet()
+        {
+            foreach (String Nimi in Kappale)
+            {
+                Console.WriteLine("--Nimi: " + Nimi);
+            }
+        }
+    }
+}
---
+++ Harkat/Harjoitus 4 (KT)/AlbumiHarjoitus/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AlbumiHarjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Albumi Alpumi = new Albumi("Artisti_123", "Musiikki_123", "Pop", 10);
+            Alpumi.lisääKappale("kappale1");
+            Alpumi.lisääKappale("kappale2");
+            Alpumi.lisääKappale("kappale3");
+            Alpumi.lisääKappale("kappale4");
+            Alpumi.lisääKappale("kappale5");
+
+            Alpumi.TulostaAlbumiData();
+            Alpumi.TulostaKappaleet();
+        }
+    }
+}
