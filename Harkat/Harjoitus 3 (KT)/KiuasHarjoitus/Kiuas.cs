7469ac0693f602364a0e08115bd7af61f8664b33
niilo23 <56909449+niilo23@users.noreply.github.com>
2021-01-22 11:56:09 +0200

    Kiuas tehtävä valmis

---
+++ Harkat/Harjoitus 3 (KT)/KiuasHarjoitus/Kiuas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KiuasHarjoitus
+{
+    class Kiuas
+    {
+        public bool päällä;
+        public float lämpö;
+        public float kosteus;
+
+        public Kiuas (bool _OnOff, float _lämpö, float _kosteus)
+        {
+            päällä = _OnOff;
+            lämpö = _lämpö;
+            kosteus = _kosteus;
+        }
+
+        public void KiuasData()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Kiuas päällä: " + päällä);
+            Console.WriteLine("Kiukaan lämpö: " + lämpö + "°C");
+            Console.WriteLine("Kiukaan kostues: " + kosteus + "%");
+            Console.WriteLine("");
+        }
+
+        public void KiuasPäällePois(bool p)
+        {
+            päällä = p;
+        }
+
+        public void SäädäLämpö(int i)
+        {
+            lämpö += i;
+        }
+
+        public void SäädäKosteus(int i)
+        {
+            kosteus += i;
+        }
+    }
+}
--- Harkat/Harjoitus 3 (KT)/KiuasHarjoitus/Program.cs
+++ Harkat/Harjoitus 3 (KT)/KiuasHarjoitus/Program.cs
@@ -6,7 +6,23 @@ namespace KiuasHarjoitus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kiuas Harvia = new Kiuas(false, 20, 0);
+
+            Harvia.KiuasData();
+            Harvia.KiuasPäällePois(true);
+            Harvia.KiuasData();
+
+            Harvia.SäädäLämpö(40);
+            Harvia.SäädäKosteus(60);
+            Harvia.KiuasData();
+
+            Harvia.SäädäKosteus(-20);
+            Harvia.KiuasData();
+
+            Harvia.SäädäLämpö(-40);
+            Harvia.SäädäKosteus(-40);
+            Harvia.KiuasPäällePois(false);
+            Harvia.KiuasData();
         }
     }
 }
